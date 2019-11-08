7ec0d17baf5e9ee0d19a2cb73b2b7ffd18fab571
Mário Prada <mariosilvaprada@gmail.com>
2019-11-08 23:15:25 +0000

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [ toppings, setToppings ] = useState('');
+
+  const [ combinations, setCombinations ] = useState('');
+  const [ rankIngredients, setRankIngredients ] = useState('');
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const result = await axios.get('http://files.olo.com/pizzas.json');
+
+    setToppings(result.data);
+
+    let combinations = [];
+
+    result.data.map((topping) => {
+      if (topping.toppings.length > 1) {
+        combinations.push(topping.toppings);
+      }
+    });
+
+    let numCombination = {};
+
+    combinations.map((el) => {
+      if (numCombination[el]) {
+        numCombination[el]++;
+      } else {
+        numCombination[el] = 1;
+      }
+    });
+
+    let sortCombination = Object.keys(numCombination).sort((a, b) => {
+      return numCombination[b] - numCombination[a];
+    });
+
+    setCombinations(sortCombination);
+
+    let ranking = {};
+    result.data.map((topping) => {
+      if (topping.toppings.length > 1) {
+        topping.toppings.map((top) => {
+          ranking[top] ? ranking[top]++ : (ranking[top] = 1);
+        });
+      } else {
+        ranking[topping] ? ranking[topping]++ : (ranking[topping] = 1);
+      }
+    });
+
+    // sort topping by order amount
+    let sortTopping = Object.keys(ranking).sort((a, b) => {
+      return ranking[b] - ranking[a];
+    });
+    setRankIngredients(sortTopping);
+    console.log(sortTopping);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    toppings &&
+    rankIngredients &&
+    combinations && (
+      <div className='App'>
+        <h1>Top order toppings:</h1>
+        {rankIngredients.map((ing, i) => i > 0 && <p key={i}>{ing}</p>)}
+        <h1>Top order combinations:</h1>
+        {combinations.map((comb, i) => <p key={i}>{comb}</p>)}
+      </div>
+    )
   );
 }
 
